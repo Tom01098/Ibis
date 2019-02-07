d98ec23f72bd422b2a748dd30b8f388a69e2d433
Tom Humphreys <Tom01098Code@gmail.com>
2019-02-07 11:22:07 +0000

    Get rule name and body

--- Ibis.Tests/GetRulesTests.cs
+++ Ibis.Tests/GetRulesTests.cs
@@ -9,20 +9,20 @@ namespace Ibis.Tests
         [TestMethod]
         public void Single()
         {
-            var grammar = "binary_digit = '0' | '1'";
+            var grammar = "A = 'A'; ";
 
             var rules = new Parser().GetRules(grammar);
 
             Assert.AreEqual(1, rules.Count);
-            Assert.AreEqual("binary_digit ", rules[0].name);
-            Assert.AreEqual(" '0' | '1'", rules[0].body);
+            Assert.AreEqual("A ", rules[0].name);
+            Assert.AreEqual(" 'A'", rules[0].body);
         }
 
         [TestMethod]
         public void Multiple()
         {
-            var grammar = @"binary_digit = '0' | '1'
-                            binary_number = binary_digit{binary_digit}";
+            var grammar = @"binary_digit = '0' | '1';
+                            binary_number = binary_digit{binary_digit};";
 
             var rules = new Parser().GetRules(grammar);
 
@@ -36,13 +36,13 @@ namespace Ibis.Tests
         [TestMethod]
         public void Multiple2()
         {
-            var grammar = @"optional = '[' rule_body ']'
+            var grammar = @"optional = '[' rule_body ']';
 
-                            repetition = '{' rule_body '}'
+                            repetition = '{' rule_body '}';
 
-                            grouping = '(' rule_body ')'
+                            grouping = '(' rule_body ')';
 
-                            literal = ''' (character | number) {character | number} '''";
+                            literal = ''' (character | number) {character | number} ''';";
 
             var rules = new Parser().GetRules(grammar);
 
@@ -50,67 +50,11 @@ namespace Ibis.Tests
             Assert.AreEqual("optional ", rules[0].name);
             Assert.AreEqual(" '[' rule_body ']'", rules[0].body);
             Assert.AreEqual("repetition ", rules[1].name);
-            Assert.AreEqual(" '(' rule_body ')'", rules[1].body);
+            Assert.AreEqual(" '{' rule_body '}'", rules[1].body);
             Assert.AreEqual("grouping ", rules[2].name);
-            Assert.AreEqual(" '{' rule_body '}'", rules[2].body);
+            Assert.AreEqual(" '(' rule_body ')'", rules[2].body);
             Assert.AreEqual("literal ", rules[3].name);
-            Assert.AreEqual(" ''' (character | number) {character | number} '''", rules[4].body);
-        }
-
-        [TestMethod]
-        public void Multiple3()
-        {
-            var grammar = @"whitespace = ? whitespace characters ? ;
-
-character = ? a-z and A-Z ? ;
-
-number = ? 0-9 ? ;
-
-
-
-rule = name [whitespace] '=' rule_body [whitespace] ';' ;
-
-name = character {character | number} ;
-
-rule_body = [whitespace] rule_statement {whitespace '|' whitespace rule_statement} [whitespace] ;
-
-rule_statement = optional | repetition | grouping | literal | name ;
-
-
-
-optional = '[' rule_body ']' ;
-
-repetition = '{' rule_body '}' ;
-
-grouping = '(' rule_body ')' ;
-
-literal = ''' (character | number) {character | number} ''' ;";
-
-            var rules = new Parser().GetRules(grammar);
-
-            Assert.AreEqual(11, rules.Count);
-            Assert.AreEqual("whitespace ", rules[0].name);
-            Assert.AreEqual(" ? whitespace characters ?", rules[0].body);
-            Assert.AreEqual("character ", rules[1].name);
-            Assert.AreEqual(" ? a-z and A-Z ?", rules[1].body);
-            Assert.AreEqual("number ", rules[2].name);
-            Assert.AreEqual(" ? 0-9 ?", rules[2].body);
-            Assert.AreEqual("rule ", rules[3].name);
-            Assert.AreEqual(" name [whitespace] '=' rule_body [whitespace] ';'", rules[3].body);
-            Assert.AreEqual("name ", rules[4].name);
-            Assert.AreEqual(" character {character | number}", rules[4].body);
-            Assert.AreEqual("rule_body ", rules[5].name);
-            Assert.AreEqual(" [whitespace] rule_statement {whitespace '|' whitespace rule_statement}", rules[5].body);
-            Assert.AreEqual("rule_statement ", rules[6].name);
-            Assert.AreEqual(" optional | repetition | grouping | literal | name", rules[6].body);
-            Assert.AreEqual("optional ", rules[7].name);
-            Assert.AreEqual(" '[' rule_body ']'", rules[7].body);
-            Assert.AreEqual("repetition ", rules[8].name);
-            Assert.AreEqual(" '{' rule_body '}'", rules[8].body);
-            Assert.AreEqual("grouping ", rules[9].name);
-            Assert.AreEqual(" '(' rule_body ')'", rules[9].body);
-            Assert.AreEqual("literal ", rules[10].name);
-            Assert.AreEqual(" ''' (character | number) {character | number} '''", rules[10].body);
+            Assert.AreEqual(" ''' (character | number) {character | number} '''", rules[3].body);
         }
     }
 }
--- Ibis/EBNF/Parser.cs
+++ Ibis/EBNF/Parser.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using static System.Char;
 
 namespace Ibis.EBNF
 {
@@ -11,11 +13,62 @@ namespace Ibis.EBNF
             return null;
         }
 
-        internal List<(string name, string body)> GetRules(string grammar)
+        // Get the rules from a given grammar
+        public List<(string name, string body)> GetRules(string grammar)
         {
             var rules = new List<(string name, string body)>();
+            var span = grammar.AsSpan();
 
+            int index = 0;
+            int current = 0;
 
+            // Iterate through the grammar
+            while (index < span.Length)
+            {
+                // Ignore starting whitespace
+                while (IsWhiteSpace(span[index]))
+                {
+                    index++;
+                    current++;
+
+                    // Skip to the end
+                    if (index == span.Length)
+                    {
+                        // Lovely use of a goto
+                        goto end;
+                    }
+                }
+
+                // Find the '='
+                while (span[index] != '=')
+                {
+                    index++;
+                }
+
+                // Split to get rule name
+                var name = span.Slice(current, index - current).ToString();
+
+                index++;
+                current = index;
+                
+                // Find the ';'
+                while (span[index] != ';')
+                {
+                    index++;
+                }
+
+                // Split to get rule body
+                var body = span.Slice(current, index - current).ToString();
+
+                index++;
+                current = index;
+
+                // Add rule name and body
+                rules.Add((name, body));
+            }
+
+            // Label for goto
+            end:
 
             return rules;
         }
