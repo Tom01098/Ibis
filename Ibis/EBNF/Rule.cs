3c6eb449cb0fdf2da99b8e696fac3ff2a12e691f
Tom Humphreys <Tom01098Code@gmail.com>
2019-02-09 20:54:15 +0000

    Added rules

--- Ibis/EBNF/Rule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Ibis.EBNF
-{
-    internal abstract class Rule
-    {
-        public abstract bool IsValid(ReadOnlySpan<char> span);
-    }
-}
---
+++ Ibis/EBNF/Rules.cs
@@ -0,0 +1,55 @@
+﻿namespace Ibis.EBNF
+{
+    internal abstract class Rule
+    {
+        internal class Body : Rule
+        {
+            private readonly Rule[] rules;
+
+            public Body(Rule[] rules)
+            {
+                this.rules = rules;
+            }
+        }
+
+        internal class Optional : Rule
+        {
+            private readonly Body body;
+
+            public Optional(Body body)
+            {
+                this.body = body;
+            }
+        }
+
+        internal class Repetition : Rule
+        {
+            private readonly Body body;
+
+            public Repetition(Body body)
+            {
+                this.body = body;
+            }
+        }
+
+        internal class Grouping : Rule
+        {
+            private readonly Body body;
+
+            public Grouping(Body body)
+            {
+                this.body = body;
+            }
+        }
+
+        internal class Literal : Rule
+        {
+            private readonly string literal;
+
+            public Literal(string literal)
+            {
+                this.literal = literal;
+            }
+        }
+    }
+}
