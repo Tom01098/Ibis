cc916ee3af970c1de647f8ae07bacfe0e2d2240c
Tom Humphreys <Tom01098Code@gmail.com>
2019-02-13 19:14:19 +0000

    Added a TokenEnumerator for backtracking.

---
+++ Ibis.Tests/TokenEnumeratorTests.cs
@@ -0,0 +1,87 @@
+﻿using Ibis.EBNF.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Ibis.Tests
+{
+    [TestClass]
+    public class TokenEnumeratorTests
+    {
+        [TestMethod]
+        public void Enumerate()
+        {
+            var tokens = new List<Token>
+            {
+                new IdentifierToken("x"),
+                new SymbolToken(SymbolType.Equals),
+                new IdentifierToken("3")
+            };
+
+            var enumerator = new TokenEnumerator(tokens.ToArray());
+
+            Assert.AreEqual(new IdentifierToken("x"), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            Assert.AreEqual(new SymbolToken(SymbolType.Equals), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            Assert.AreEqual(new IdentifierToken("3"), enumerator.Current);
+            Assert.AreEqual(false, enumerator.MoveNext());
+            Assert.AreEqual(null, enumerator.Current);
+        }
+
+        [TestMethod]
+        public void Backtracking()
+        {
+            var tokens = new List<Token>
+            {
+                new IdentifierToken("x"),
+                new SymbolToken(SymbolType.Equals),
+                new IdentifierToken("3")
+            };
+
+            var enumerator = new TokenEnumerator(tokens.ToArray());
+
+            Assert.AreEqual(new IdentifierToken("x"), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            enumerator.SetBacktrackPoint();
+            Assert.AreEqual(new SymbolToken(SymbolType.Equals), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            enumerator.Backtrack();
+            Assert.AreEqual(new SymbolToken(SymbolType.Equals), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            Assert.AreEqual(new IdentifierToken("3"), enumerator.Current);
+            Assert.AreEqual(false, enumerator.MoveNext());
+            Assert.AreEqual(null, enumerator.Current);
+        }
+
+        [TestMethod]
+        public void NestedBacktracking()
+        {
+            var tokens = new List<Token>
+            {
+                new IdentifierToken("x"),
+                new SymbolToken(SymbolType.Equals),
+                new IdentifierToken("3")
+            };
+
+            var enumerator = new TokenEnumerator(tokens.ToArray());
+
+            Assert.AreEqual(new IdentifierToken("x"), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            enumerator.SetBacktrackPoint();
+            Assert.AreEqual(new SymbolToken(SymbolType.Equals), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            enumerator.SetBacktrackPoint();
+            Assert.AreEqual(new IdentifierToken("3"), enumerator.Current);
+            Assert.AreEqual(false, enumerator.MoveNext());
+            enumerator.Backtrack();
+            Assert.AreEqual(new IdentifierToken("3"), enumerator.Current);
+            Assert.AreEqual(false, enumerator.MoveNext());
+            enumerator.Backtrack();
+            Assert.AreEqual(new SymbolToken(SymbolType.Equals), enumerator.Current);
+            Assert.AreEqual(true, enumerator.MoveNext());
+            Assert.AreEqual(new IdentifierToken("3"), enumerator.Current);
+            Assert.AreEqual(false, enumerator.MoveNext());
+            Assert.AreEqual(null, enumerator.Current);
+        }
+    }
+}
---
+++ Ibis/EBNF/Tokens/TokenEnumerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace Ibis.EBNF.Tokens
+{
+    internal class TokenEnumerator
+    {
+        private readonly Token[] tokens;
+        private readonly Stack<int> indexes;
+
+        public TokenEnumerator(Token[] tokens)
+        {
+            this.tokens = tokens;
+            indexes = new Stack<int>();
+            indexes.Push(0);
+        }
+
+        public Token Current
+        {
+            get
+            {
+                if (indexes.Peek() >= tokens.Length)
+                {
+                    return null;
+                }
+
+                return tokens[indexes.Peek()];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            indexes.Push(indexes.Pop() + 1);
+            return indexes.Peek() < tokens.Length;
+        }
+
+        public void SetBacktrackPoint()
+        {
+            indexes.Push(indexes.Peek());
+        }
+
+        public void Backtrack()
+        {
+            indexes.Pop();
+        }
+    }
+}
