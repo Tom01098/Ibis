7a25ef72d06f00e11647aa8b6f3264a97a1361e8
Tom Humphreys <Tom01098Code@gmail.com>
2019-02-21 20:30:16 +0000

    Validate method infrastructure, cases need to be implemented

--- Ibis/Validation/Validator.cs
+++ Ibis/Validation/Validator.cs
@@ -6,11 +6,12 @@ namespace Ibis.Validation
 {
     internal class Validator
     {
+        private Dictionary<string, RuleBody> rules = new Dictionary<string, RuleBody>();
+        private ProgramEnumerator enumerator;
+
         public bool Validate(List<Rule> rulesList, string program)
         {
-            var enumerator = new ProgramEnumerator(program);
-
-            var rules = new Dictionary<string, RuleBody>();
+            enumerator = new ProgramEnumerator(program);
 
             foreach (var rule in rulesList)
             {
@@ -26,7 +27,64 @@ namespace Ibis.Validation
                 throw new ArgumentException("Grammar does not contain a 'main' rule");
             }
 
-            throw new NotImplementedException();
+            return Validate(rules["main"]);
+        }
+
+        private bool Validate(object obj)
+        {
+            // RuleBody handling
+            if (obj is RuleBody ruleBody)
+            {
+                foreach (var ruleSection in ruleBody.RuleSections)
+                {
+                    if (Validate(ruleSection))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            // RuleSection handling
+            else if (obj is RuleSection ruleSection)
+            {
+                foreach (var ruleStatement in ruleSection.RuleStatements)
+                {
+                    if (!Validate(ruleStatement))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+            // Repetition handling
+            else if (obj is Repetition repetition)
+            {
+                throw new NotImplementedException();
+            }
+            // Optional handling
+            else if (obj is Optional optional)
+            {
+                throw new NotImplementedException();
+            }
+            // Grouping handling
+            else if (obj is Grouping grouping)
+            {
+                throw new NotImplementedException();
+            }
+            // Name handling
+            else if (obj is Name name)
+            {
+                throw new NotImplementedException();
+            }
+            // Literal handling
+            else if (obj is Literal literal)
+            {
+                throw new NotImplementedException();
+            }
+
+            throw new ArgumentException("Could not validate supplied object");
         }
     }
 }
